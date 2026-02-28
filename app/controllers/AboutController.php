8c1e2b48c3deba48cfffc9dd95168dd9692c8a3f
Andika Palian <andika12des@gmail.com>
2026-02-28 14:28:20 +0800

    feat: make the about page dinamic

--- app/controllers/AboutController.php
+++ app/controllers/AboutController.php
@@ -8,6 +8,8 @@ class AboutController extends Controller
     $data['judul'] = 'Tentang Bantimurung';
     $data['page'] = 'about';
 
+    $data['about'] = $this->model('AboutModel')->getAboutData();
+
     $this->view('templates/header', $data);
     $this->view('templates/navbar', $data);
     $this->view('home/about', $data);
